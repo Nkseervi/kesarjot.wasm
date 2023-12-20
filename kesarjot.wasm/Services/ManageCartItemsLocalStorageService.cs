e6db7f23e2a82a5c69abe8a57d593edf80ca4d16
Nkseervi <nkseervi@gmail.com>
2023-12-20 19:16:05 +0530

    For PhonePe PG: to be reverted

--- kesarjot.wasm/Services/ManageCartItemsLocalStorageService.cs
+++ kesarjot.wasm/Services/ManageCartItemsLocalStorageService.cs
@@ -36,7 +36,8 @@
 
         private async Task<List<CartItemDto>> AddCollection()
         {
-            var shoppingCartCollection = await this.shoppingCartService.GetItems(_loggedInUserDto.Id);
+            var shoppingCartCollection = await this.shoppingCartService.GetItems("8e9c9233-2b0f-40f6-8ca9-a331b33748ad");
+            //var shoppingCartCollection = await this.shoppingCartService.GetItems(_loggedInUserDto.Id);
 
             if (shoppingCartCollection != null)
             {
